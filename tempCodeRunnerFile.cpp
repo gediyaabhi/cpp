a8a86ec750360388c9a7dd3b9c3fe84532779953
darshan <darshan2701k@gmail.com>
2023-07-13 18:57:56 +0530

    cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
-int main ()
+int main()
 {
-    int i;
-    for(i=51;i<=99;i++)
+    int odd,n;
+
+    cout<<"enter a value";
+    cin>>n;
+
+    for(odd=1;odd<=n;odd+=2)
     {
-        cout<<" " <<i;
+        cout<<"" << odd;
     }
+
 }
