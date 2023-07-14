2ebbc5225ba45138b0beb0f7c805981e27dd8e9b
darshan <darshan2701k@gmail.com>
2023-07-14 17:52:46 +0530

    cpp

---
+++ atoz.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char i;
+    int j=65;
+    cout<< "A to Z \n";
+    for(i=65;i<=90;i++)
+    {
+        cout << " " << i;
+    cout << " " << j;
+            j++;
+    }
+}
