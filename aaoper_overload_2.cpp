09fe20ae98356de52dae2dd2cab34ab03c23e214
darshan <darshan2701k@gmail.com>
2023-07-22 18:26:06 +0530

    cpp

--- aaoper_overload_2.cpp
+++ aaoper_overload_2.cpp
@@ -1,36 +1,36 @@
 #include<iostream>
 using namespace std;
-class box {
+class box{
     int l,w,h;
     public:
     void setdata(int a, int b ,int c)
     {
-        l=a , w=b , h=c;
+        l=a ,w=b, h=c;
     }
     int getdata()
     {
         return l*w*h;
     }
-    box operator*(box &n)
+     
+    box operator-(box &n)
     {
         box t;
-        t.l = l * n.l;
-        t.w = w * n.w;
-        t.h = h * n.h;
-         
+        t.l = l - n.l;
+        t.w = w - n.w;
+        t.h = h - n.h;
         return t;
     }
 };
+
 int main ()
 {
     box a,b,c;
-
-    a.setdata(2,2,2);
-    cout << "Volume of A is :" << a.getdata() << endl;
+    a.setdata(5,5,5);
+    cout << "volume of A is :" << a.getdata() << endl;
 
     b.setdata(3,3,3);
     cout << "Volume of B is :" << b.getdata() << endl;
 
-    c=a*b;
-    cout << "VOlume of C is :" << c.getdata() << endl; 
+    c=a-b;
+    cout << "Volume of C is :" << c.getdata() << endl; 
 }
